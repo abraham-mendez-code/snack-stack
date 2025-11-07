9c156856bce6a79768b8766ddf14992922de1b27
abraham-mendez-debug <abraham.me001@gmail.com>
2025-11-07 13:11:38 -0500

    Add the Chips class

---
+++ src/main/java/com/pluralsight/models/Chips.java
@@ -0,0 +1,15 @@
+package com.pluralsight.models;
+
+public class Chips implements Valuable{
+
+    // CLASS ATTRIBUTES
+    private final double BASE_PRICE = 1.50;
+
+    // constructor
+    public Chips() {};
+
+    @Override
+    public double getValue() {
+        return BASE_PRICE;
+    }
+}

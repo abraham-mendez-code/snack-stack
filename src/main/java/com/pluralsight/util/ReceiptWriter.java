6d6d0bb78ea2ff482427baed0fa9032bbd527cc7
abraham-mendez-debug <abraham.me001@gmail.com>
2025-11-08 15:49:52 -0500

    feat(util): add ReceiptWriter to save Order summaries to file
    - Creates a file in src/main/resources/ with a timestamped filename.
    - Writes the output of Order.getSummary() to the file.
    - Handles IOException by printing the stack trace.

---
+++ src/main/java/com/pluralsight/util/ReceiptWriter.java
@@ -0,0 +1,34 @@
+package com.pluralsight.util;
+
+import com.pluralsight.models.Order;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ReceiptWriter {
+    private final String FILE_DIR = "src/main/resources/";
+
+    // constructor
+    public ReceiptWriter() {
+    }
+
+    public void saveReceipt(Order order) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String fileName = FILE_DIR + now.format(formatter) + ".txt";
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter( new FileWriter(fileName) ) ) {
+
+            bufferedWriter.write(order.getSummary());
+        }
+        catch (IOException e) {
+            System.out.println("Error writing to file ");
+            e.printStackTrace();
+        }
+
+
+    }
+}

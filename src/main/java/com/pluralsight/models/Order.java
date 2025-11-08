31964527753e97d7114be91305ff9bb37926edce
abraham-mendez-debug <abraham.me001@gmail.com>
2025-11-08 13:08:46 -0500

    refactor(order): change return type of getSandwiches, getDrinks, and getChips to List of respective objects
    Previously, these methods returned raw lists or generic types.
    Now they return strongly typed lists for better type safety and code clarity.

--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class Order implements Valuable {
 
     // class attributes
-    List<Valuable> sandwiches;
-    List<Valuable> drinks;
-    List<Valuable> chips;
+    List<Sandwich> sandwiches;
+    List<Drink> drinks;
+    List<Chips> chips;
     double total;
 
     // constructor
@@ -22,28 +22,52 @@ public class Order implements Valuable {
     }
 
     // this method adds a sandwich to the order
-    public void addSandwich (Valuable sandwich) {
+    public void addSandwich (Sandwich sandwich) {
         sandwiches.add(sandwich);
     }
 
     // this method adds a drink to the order
-    public void addDrink (Valuable drink) {
+    public void addDrink (Drink drink) {
         drinks.add(drink);
     }
 
     // this method adds chips to the order
-    public void addChips(Valuable chip) {
+    public void addChips(Chips chip) {
         this.chips.add(chip);
     }
 
 
-    // get the order total
+// GETTER METHODS
+
+    // this method returns all sandwiches in the order
+    public List<Sandwich> getSandwiches() {
+
+        return sandwiches;
+
+    }
+
+    // this method returns all drinks in the order
+    public List<Drink> getDrinks() {
+
+        return drinks;
+    }
+
+    // this method returns all chips in the order
+    public List<Chips> getChips() {
+
+        return chips;
+
+    }
+
+    
+
+    // this method calculates the order total and returns its value
     @Override
     public double getValue() {
 
         // Sum the value of each item in an order
         total += sandwiches.stream()
-                    .mapToDouble(Valuable::getValue)
+                .mapToDouble(Valuable::getValue)
                 .sum();
 
         total += drinks.stream()
@@ -55,28 +79,6 @@ public class Order implements Valuable {
                 .sum();
 
         return total;
-
-    }
-
-// GETTER METHODS
-
-    // this method returns all sandwiches in the order
-    public List<Valuable> getSandwiches() {
-
-        return sandwiches;
-
-    }
-
-    // this method returns all drinks in the order
-    public List<Valuable> getDrinks() {
-
-        return drinks;
-    }
-
-    // this method returns all chips in the order
-    public List<Valuable> getChips() {
-
-        return chips;
 
     }
 

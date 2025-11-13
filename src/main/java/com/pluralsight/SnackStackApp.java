be402e546d40cf7d56ee792e1de09f1562db7cfb
abraham-mendez-debug <abraham.me001@gmail.com>
2025-11-12 19:29:16 -0500

    feat(app): add SnackStackApp as the main entry point
    - Initializes the program and launches the UserInterface.
    - Serves as the starting point for creating and processing orders.

---
+++ src/main/java/com/pluralsight/SnackStackApp.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import com.pluralsight.ui.UserInterface;
+
+public class SnackStackApp {
+
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+
+        ui.showHomeScreen();
+    }
+}
